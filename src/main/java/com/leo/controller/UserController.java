33fdfd71d37cd2b9d3ec14c19b86fb7020166a53
leonhardt <leonhardt@05089183-c81a-c64d-910a-a3925926a748>
2018-05-25 09:46:18 +0000

    git-svn-id: https://leonhardt-pc/svn/springboot/%E6%BA%90%E7%A8%8B%E5%BA%8F@17 05089183-c81a-c64d-910a-a3925926a748

--- src/main/java/com/leo/controller/UserController.java
+++ src/main/java/com/leo/controller/UserController.java
@@ -1,12 +1,23 @@
 package com.leo.controller;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leo.domain.User;
+import com.leo.repo.UserRepository;
 
 @RestController
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@RequestMapping("/getUser")
 	public User index(){
@@ -14,5 +25,10 @@ public class UserController {
 		user.setUserName("小明");
 		user.setPassWord("123456");
 		return user;
+	}
+	
+	@RequestMapping("/getUserPage")
+	public Page<User> indexPage(){
+		return userRepository.findAll(new PageRequest(1, 2, new Sort(Direction.DESC,"id")));
 	}
 }
