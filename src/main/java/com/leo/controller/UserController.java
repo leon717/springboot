462d0cd318abc8ceacc890e9c642e613af399f3f
leonhardt <leonhardt@05089183-c81a-c64d-910a-a3925926a748>
2018-05-25 12:32:09 +0000

    git-svn-id: https://leonhardt-pc/svn/springboot/%E6%BA%90%E7%A8%8B%E5%BA%8F@18 05089183-c81a-c64d-910a-a3925926a748

--- src/main/java/com/leo/controller/UserController.java
+++ src/main/java/com/leo/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.leo.controller;
 
-import javax.persistence.EntityManager;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
