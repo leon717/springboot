dd964dd1bfee967216855485513c567b25e7cc8d
leo <717626659@qq.com>
2018-10-05 22:31:39 +0800

    线程池配置

---
+++ src/main/java/com/leo/config/ExecutorConfig.java
@@ -0,0 +1,42 @@
+package com.leo.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class ExecutorConfig {
+
+    @Value("${user.threadPool.corePoolSize}")
+    private int corePoolSize;
+
+    @Value("${user.threadPool.maxPoolSize}")
+    private int maxPoolSize;
+
+    @Value("${user.threadPool.queueCapacity}")
+    private int queueCapacity;
+
+    @Bean
+    public Executor asyncTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //配置核心线程数
+        executor.setCorePoolSize(corePoolSize);
+        //配置最大线程数
+        executor.setMaxPoolSize(maxPoolSize);
+        //配置队列大小
+        executor.setQueueCapacity(queueCapacity);
+        //配置线程池中的线程的名称前缀
+        executor.setThreadNamePrefix("AsyncTask-");
+
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
+        executor.initialize();
+        return executor;
+    }
+}
