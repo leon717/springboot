ed5eeaedd4f8301a0fac2581271f3949aace3b4e
leo <717626659@qq.com>
2018-12-20 22:23:13 +0800

    jpa example

--- src/test/java/com/leo/repo/TestUserRepository.java
+++ src/test/java/com/leo/repo/TestUserRepository.java
@@ -19,6 +19,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -215,6 +216,14 @@ public class TestUserRepository {
 	public void testQueryEnum(){
 		List<String> userName = userRepository.findUserNameByGender(Gender.MALE);
 		System.out.println(userName);
+	}
+	
+	@Test
+	public void testExample(){
+		User user = new User();
+		user.setGender(Gender.MALE);
+		List<User> list = userRepository.findAll(Example.of(user));
+		System.out.println(list);
 	}
 
 	@Test
