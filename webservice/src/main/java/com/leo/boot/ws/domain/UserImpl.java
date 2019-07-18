c4fe6fc1a804554097f03d6e16e51bf95f9000d9
leo <717626659@qq.com>
2019-07-19 00:04:49 +0800

    fix

--- webservice/src/main/java/com/leo/boot/ws/domain/UserImpl.java
+++ webservice/src/main/java/com/leo/boot/ws/domain/UserImpl.java
@@ -1,11 +1,8 @@
 package com.leo.boot.ws.domain;
 
-import javax.jws.WebService;
-
 import org.springframework.stereotype.Service;
 
 @Service("user")
-@WebService
 public class UserImpl implements User {
 
     @Override
