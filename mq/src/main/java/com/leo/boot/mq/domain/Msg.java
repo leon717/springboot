562fc2a5f780f02792042d0923db976f13431678
leo <717626659@qq.com>
2019-08-15 22:58:14 +0800

    自定义mq序列化方式

--- mq/src/main/java/com/leo/boot/mq/domain/Msg.java
+++ mq/src/main/java/com/leo/boot/mq/domain/Msg.java
@@ -1,6 +1,5 @@
 package com.leo.boot.mq.domain;
 
-import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
@@ -8,9 +7,8 @@ import lombok.experimental.Accessors;
 
 @Accessors(chain = true)
 @Data
-public class Msg<T> implements Serializable{
-	private static final long serialVersionUID = 1L;
-	private String code;
-	private String msg;
-	private List<T> data;
+public class Msg<T> {
+    private String code;
+    private String msg;
+    private List<T> data;
 }
