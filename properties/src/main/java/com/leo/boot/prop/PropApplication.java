c63b1c7a9ba3a453dae48a463e2cee6eb335ed61
leo <717626659@qq.com>
2019-08-23 14:33:20 +0800

    prop

---
+++ properties/src/main/java/com/leo/boot/prop/PropApplication.java
@@ -0,0 +1,12 @@
+package com.leo.boot.prop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropApplication.class, args);
+	}
+}
---
+++ properties/src/main/java/com/leo/boot/prop/domain/Prop.java
@@ -0,0 +1,9 @@
+package com.leo.boot.prop.domain;
+
+import lombok.Data;
+
+@Data
+public class Prop {
+    private String name;
+    private String value;
+}
