4204156facb547676a411668f4f97e7c0ab593a2
leo <717626659@qq.com>
2019-08-29 00:16:41 +0800

    del old version

--- src/main/java/com/leo/repo/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.leo.domain.Address;
-
-public interface AddressRepository extends JpaRepository<Address, String> {
-
-}
