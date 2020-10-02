c0944075c0d114fd57c3a96172575703863b5693
leo <717626659@qq.com>
2020-10-02 20:15:45 +0800

    update

--- updown/src/main/java/com/leo/boot/updown/controller/UpdownController.java
+++ updown/src/main/java/com/leo/boot/updown/controller/UpdownController.java
@@ -1,5 +1,14 @@
 package com.leo.boot.updown.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -10,32 +19,22 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 @RestController
 public class UpdownController {
-	
-	@Value("${folder}")
+
+    @Value("${folder}")
     private String folder;
-    
+
     @GetMapping("/index")
     public List<?> index() throws IOException {
-    	File file = new File(folder);
-    	if (!file.exists()) {
-    		file.mkdirs();
-		}
-    	List<String> list = Arrays.stream(file.listFiles())
-    			.filter(File::isFile)
-    			.map(File::getName)
-    			.collect(Collectors.toList());
+        File file = new File(folder);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        List<String> list = Arrays.stream(file.listFiles())
+                .filter(File::isFile)
+                .map(File::getName)
+                .collect(Collectors.toList());
         return list;
     }
 
@@ -45,13 +44,12 @@ public class UpdownController {
         Files.write(path, file.getBytes());
         return "success";
     }
-    
+
     @GetMapping("/download")
-    public void download(@RequestParam String fileName, HttpServletRequest request, HttpServletResponse response) throws Exception {
-    	String encodeName = URLEncoder.encode(fileName, "UTF-8");
-    	response.addHeader("Content-Disposition", "attachment;fileName=" + encodeName+";fileName*=utf-8''" + encodeName);// 设置文件名
-        File file = new File(folder+fileName);
-        Files.copy(file.toPath(), response.getOutputStream());
+    public File download(@RequestParam String fileName, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String encodeName = URLEncoder.encode(fileName, "UTF-8");
+        response.addHeader("Content-Disposition", "attachment;fileName=" + encodeName + ";fileName*=utf-8''" + encodeName);// 设置文件名
+        return new File(folder + fileName);
     }
 
 }
