dd4f496b562674434e94619fefd7c3f154a2b138
leo <717626659@qq.com>
2021-01-17 19:38:39 +0800

    update projection converter

--- jpa/src/main/java/com/leo/boot/jpa/config/ConverterConfig.java
+++ jpa/src/main/java/com/leo/boot/jpa/config/ConverterConfig.java
@@ -5,16 +5,15 @@ import com.google.common.base.Converter;
 import com.leo.boot.jpa.domain.projection.UserVO;
 import org.springframework.cglib.beans.BeanMap;
 import org.springframework.cglib.core.ReflectUtils;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.GenericConverter;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.core.convert.support.GenericConversionService;
-import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -24,14 +23,14 @@ public class ConverterConfig {
 
     private Converter<String, String> caseConverter = CaseFormat.UPPER_UNDERSCORE.converterTo(CaseFormat.LOWER_CAMEL);
 
+    private GenericConversionService genericConversionService = ((GenericConversionService) DefaultConversionService.getSharedInstance());
+
     @PostConstruct
     public void registry() {
-        GenericConversionService genericConversionService = ((GenericConversionService) DefaultConversionService.getSharedInstance());
-        genericConversionService.addConverter(dbProjectionConverter());
+        genericConversionService.addConverter(projectionConverter());
     }
 
-    @Bean
-    public GenericConverter dbProjectionConverter() {
+    public GenericConverter projectionConverter() {
         return new GenericConverter() {
 
             @Override
@@ -46,16 +45,11 @@ public class ConverterConfig {
 
                 ((Map<String, Object>) source).forEach((key, value) -> {
                     String propertyName = caseConverter.convert(key);
-                    if (!beanMap.containsKey(propertyName)) {
+                    if (!beanMap.containsKey(propertyName) || Objects.isNull(key)) {
                         return;
                     }
-
-                    Class propertyType = beanMap.getPropertyType(propertyName);
-                    if (Enum.class.isAssignableFrom(propertyType) && !StringUtils.isEmpty(value.toString())) {
-                        beanMap.put(propertyName, Enum.valueOf(propertyType, value.toString()));
-                    } else {
-                        beanMap.put(propertyName, value);
-                    }
+                    Object propertyValue = genericConversionService.convert(value, beanMap.getPropertyType(propertyName));
+                    beanMap.put(propertyName, propertyValue);
                 });
                 return target;
             }
